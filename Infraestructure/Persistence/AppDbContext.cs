1e6d2dfab65f322eac7f3df21181ec7f9a23a5fd
LeonelEstrella <ealanleonel@gmail.com>
2024-04-15 14:15:24 -0300

    Implement template

---
+++ Infraestructure/Persistence/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infraestructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
